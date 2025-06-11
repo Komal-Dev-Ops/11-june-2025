ebbf65330387fcfdb0402ba08d3f81075b5ce44a
Komal <komalbhor2017@gmail.com>
2025-06-11 07:44:51 +0000

    ewpo

---
+++ src/config.py
@@ -0,0 +1,11 @@
+
+MILVUS_CONFIG = {
+    "host": "172.31.16.237",
+    "port": "19530",
+    "collection_name": "LangChainCollection1"
+}
+
+#EMBEDDING_MODEL_NAME = "BAAI/bge-small-en"
+EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
+OLLAMA_MODEL_NAME = "llama3.2"
+OLLAMA_URL = "http://127.0.0.1:11434"
